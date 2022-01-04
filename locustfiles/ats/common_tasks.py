88994858e2effcab7397984bde9718b70c311217
mekan.allaberdi <mekan.allaberdi@gmail.com>
2022-01-04 23:05:14 +0500

    changes common ats

--- locustfiles/ats/common_tasks.py
+++ locustfiles/ats/common_tasks.py
@@ -4,14 +4,14 @@ def list_candidates(tag_name, limit=None):
             user = args[0].user
             query_params = func(*args, **kwargs)
             if limit is not None:
-                query_params["limit"] = limit
-            res = user.client.get(
+                query_params["limit"] = 1000
+                query_params["offset"] = 3100
+            user.client.get(
                 "/candidates",
                 headers=user.get_auth_headers(),
                 params=query_params,
                 name="/candidates:{}".format(tag_name),
             )
-            print(res.text)
 
         return wrapped_func
 
